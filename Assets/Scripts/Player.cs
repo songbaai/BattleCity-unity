1638d5921b83349135956fe7e71eb7cb56d92657
song <sorabai@outlook.com>
2020-05-06 21:35:56 -0700

    initial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");//left -1 right 1
+        transform.Translate(Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
+        
+
+        float v = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.up * v * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
